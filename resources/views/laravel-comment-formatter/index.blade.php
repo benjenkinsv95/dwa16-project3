e64b9645515270017f634f45a7855048eaca29d8
Ben Jenkins <benjenkinsv95@gmail.com>
2016-10-25 08:41:42 -0500

    Resloving w3 warnings.

--- resources/views/laravel-comment-formatter/index.blade.php
+++ resources/views/laravel-comment-formatter/index.blade.php
@@ -8,47 +8,42 @@
 @endsection
 
 @section('content')
-<article>
+<div class="row">
+    <form class="col-xs-12 col-sm-11 col-md-8 col-lg-5" method="POST" action="{{ url('/laravel_comment_formatter') }}">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <fieldset>
+            @include('forms.form_group', ['id' => 'title'])
+                <label for="title" class="control-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="{{ $PLACEHOLDER_TITLE }}" value='{{ old('title') }}' required>
+                @if($errors->get('title'))
+                     @include('errors.list', ['id' => 'title'])
+                @endif
+            @include('forms.end_form_group')
 
-    <div class="row">
-        <form class="col-xs-12 col-sm-11 col-md-8 col-lg-5" method="POST" action="{{ url('/laravel_comment_formatter') }}">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <fieldset>
-                @include('forms.form_group', ['id' => 'title'])
-                    <label for="title" class="control-label">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="{{ $PLACEHOLDER_TITLE }}" value='{{ old('title') }}' required>
-                    @if($errors->get('title'))
-                         @include('errors.list', ['id' => 'title'])
-                    @endif
-                @include('forms.end_form_group')
+            @include('forms.form_group', ['id' => 'comment'])
+                <label for="comment" class="control-label">Comment</label>
+                <textarea class="form-control" rows="3" id="comment" name="comment" placeholder="{{ $PLACEHOLDER_COMMENT }}" required>{{ old('comment') }}</textarea>
+                @if($errors->get('comment'))
+                    @include('errors.list', ['id' => 'comment'])
+                @endif
+            @include('forms.end_form_group')
 
-                @include('forms.form_group', ['id' => 'comment'])
-                    <label for="comment" class="control-label">Comment</label>
-                    <textarea class="form-control" rows="3" id="comment" name="comment" placeholder="{{ $PLACEHOLDER_COMMENT }}" required>{{ old('comment') }}</textarea>
-                    @if($errors->get('comment'))
-                        @include('errors.list', ['id' => 'comment'])
-                    @endif
-                @include('forms.end_form_group')
-
-                <div class="row">
-                    <div class="col-xs-12 col-sm-5">
-                        <div class="form-group">
-                            <input class="btn btn-primary btn-block" type="submit" value="Format">
-                        </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-5">
+                    <div class="form-group">
+                        <input class="btn btn-primary btn-block" type="submit" value="Format">
                     </div>
                 </div>
-            </fieldset>
-        </form>
-
-        <!-- Show password if it exists-->
-        @if(!empty(trim($formattedComment)))
-            <div class="col-xs-12 col-sm-11 col-md-8 col-lg-7">
-                <pre>{{ $formattedComment }}</pre>
             </div>
-        @endif
-    </div>
+        </fieldset>
+    </form>
 
-
-</article>
+    <!-- Show password if it exists-->
+    @if(!empty(trim($formattedComment)))
+        <div class="col-xs-12 col-sm-11 col-md-8 col-lg-7">
+            <pre>{{ $formattedComment }}</pre>
+        </div>
+    @endif
+</div>
 @endsection
 
